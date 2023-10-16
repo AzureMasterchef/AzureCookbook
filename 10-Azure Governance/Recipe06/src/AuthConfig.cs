d612474791f439ced996b753ba7d47fe8445679a
Massimo Bonanni <massimo.bonanni@tiscali.it>
2023-10-16 16:05:39 +0200

    Migration from ADO to GitHub

---
+++ 10-Azure Governance/Recipe06/src/AuthConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace src
+{
+    internal class AuthConfig
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string TenantId { get; set; }
+
+        public void Load(IConfiguration configuration)
+        {
+            this.ClientId = configuration["ClientId"];
+            this.ClientSecret = configuration["ClientSecret"];
+            this.TenantId = configuration["TenantId"];
+        }
+    }
+}
