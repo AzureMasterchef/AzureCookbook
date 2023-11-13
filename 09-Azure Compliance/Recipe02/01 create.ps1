8e132661cfecf9efdc9c82dfd20dfe9ebeb8ec9f
Marco Obinu <marco.obinu@omegamadlab.com>
2023-11-13 21:28:45 +0100

    final update 09.02

--- 09-Azure Compliance/Recipe02/01 create.ps1
+++ 09-Azure Compliance/Recipe02/01 create.ps1
@@ -32,40 +32,19 @@ $templateSpec_v1 = New-AzTemplateSpec -Name "$envPrefix-landingZone-ts" `
 
 # Create a deployment stack starting from the template spec. The deployment stack targets the subscription, since landing_zone.bicep create a resource group at that scope
 $storageAccountName = "recipe0902stg$(Get-Random -Minimum 000000 -Maximum 999999)"
+$storageAccountName | Out-File storageAccountName.txt
 
 $parameters = @{
     rgName = "$envPrefix-target-rg"
     storageAccountName = $storageAccountName
-    $location = $location
+    location = $location
 }
 
 New-AzSubscriptionDeploymentStack  `
     -Name "$envPrefix-DeploymentStack" `
+    -location $location `
     -TemplateSpec $templateSpec_v1.versions.Id `
     -TemplateParameterObject $parameters `
     -DeleteAll `
     -DenySettingsApplyToChildScopes `
     -DenySettingsMode "DenyDelete"
-
-# !!!!!!!!!!!!!!!!!!
-# Execute the remaing part of the script after applying a change to landing_zone.bicep
-
-# Generate a new version of the Template Spec
-$templateSpec_v2 = New-AzTemplateSpec -Name "$envPrefix-landingZone-ts" `
-                    -Version 2.0 `
-                    -ResourceGroupName $rg.ResourceGroupName `
-                    -Location $location `
-                    -TemplateFile ./landing_zone.bicep
-
-Set-AzSubscriptionDeploymentStack  `
-    -Name "$envPrefix-DeploymentStack" `
-    -DeploymentResourceGroupName  "$envPrefix-managed-RG" `
-    -TemplateSpec $templateSpec_v2.versions.Id `
-    -TemplateParameterObject $parameters `
-    -DeleteAll `
-    -DenySettingsApplyToChildScopes `
-    -DenySettingsMode "DenyDelete"
-
-# Delete the deployment stack and all the related resources
-Remove-AzSubscriptionDeploymentStack `
-  -Name "$envPrefix-DeploymentStack"
